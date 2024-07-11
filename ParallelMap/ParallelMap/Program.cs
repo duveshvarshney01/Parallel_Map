3422aca1db370806be6216a3ac216b9542fcb62a
duveshvarshney01 <46156313+duveshvarshney01@users.noreply.github.com>
2024-07-11 22:50:45 +0530

    Add files via upload

---
+++ ParallelMap/ParallelMap/Program.cs
@@ -0,0 +1,28 @@
+﻿using ParallelMap;
+
+Func<int>[] funcs = new Func<int>[]
+{
+        () => { Thread.Sleep(1000); return 1; },
+        () => { Thread.Sleep(500); return 2; },
+        () => { Thread.Sleep(2000); return 3; },
+        () => { Thread.Sleep(500); return 4; },
+};
+
+int N = 4;
+int timeoutMs = 5000;
+
+try
+{ 
+    var results = await ParallelMapper.PMapN<int>(N, funcs, timeoutMs);
+
+    Console.WriteLine("Results:");
+    foreach (var result in results)
+    {
+        Console.WriteLine(result);
+    }
+    Console.ReadLine();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
